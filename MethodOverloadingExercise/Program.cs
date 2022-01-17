e25f947235027ed4ba46b49165dfd649b35b8b72
Jaylon Carter <carterjay2001@outlook.com>
2022-01-17 11:36:17 -0600

    initial commit

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MethodOverloadingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(5, 6));
+            Console.WriteLine(Add(12.56m, 16.27m));
+            Console.WriteLine(Add(14, 17, true));
+
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool money)
+        {
+            int ans = num1 + num2;
+
+            if (money)
+            {
+                if (ans == 1)
+                {
+                    return $"{ans} dollar";
+                }
+                else
+                {
+                    return $"{ans} dollars";
+                }
+            }
+            else
+            {
+            return $"{ans}";
+            }
+        }
+    }
+}
